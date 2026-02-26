a34bd139967e2a7868f1814a169844337c2a773e
Eng-adam <aljadadm654@gmail.com>
2026-02-26 18:42:46 +0200

    chore: remove temp files

--- tmp_fix.sql
+++
@@ -1,2 +0,0 @@
-UPDATE users SET site_key='nexiroflux' WHERE id=20;
-SELECT id,name,email,role,site_key FROM users WHERE id=20;
--- tmp_gen_token.js
+++
@@ -1,4 +0,0 @@
-const jwt = require("jsonwebtoken");
-const { JWT_SECRET } = require("./src/config/env");
-const t = jwt.sign({ id: 20, role: "admin", site_key: "local-dev" }, JWT_SECRET);
-console.log(t);
